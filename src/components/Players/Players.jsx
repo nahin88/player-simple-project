b3565202c2108df432642df3b6a18c0f94266a24
nahin88 <nahiraja88@gmail.com>
2023-04-01 21:14:03 +0600

    sidecart added

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Player from "../Player/Player";
+import Sideber from "../Sideber/Sideber";
 
 const Players = () => {
-    const [players, setPlayers] = useState([]);
-    useEffect(() => {
-        // fetch('data.json').then(res=>res.json()).then(data => console.log(data))
-        const fetchingData = async() => {
-            const res = await fetch('data.json');
-            const data =await res.json();
-            setPlayers(data)
-        }
-        fetchingData();
-    },[])
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    // fetch('data.json').then(res=>res.json()).then(data => console.log(data))
+    const fetchingData = async () => {
+      const res = await fetch("data.json");
+      const data = await res.json();
+      setPlayers(data);
+    };
+    fetchingData();
+  }, []);
   return (
-    <div className="players-container flex w-4/5">
-      <div className="players grid grid-cols-3 gap-5 ml-5">
-       <Player players={players}></Player>
+    <div className="players-container flex my-12">
+      <div className="players grid grid-cols-3 gap-5 ml-5 w-3/4">
+        <Player players={players}></Player>
       </div>
       {/* players info  */}
-      <div className="players-info w-1/5"></div>
+      <div className="players-info">
+        <Sideber></Sideber>
+      </div>
     </div>
   );
 };
---
+++ src/components/Sideber/Sideber.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sideber = () => {
+  return (
+    <div>
+      <div className="bg-gray-300  p-8 ml-5">
+        <h2>totalPlayer added: {}</h2>
+        <h1>Total price: {}</h1>
+      </div>
+      <div className="bg-rose-300 mt-5 ml-5 p-8">
+        <h1 className="font-bold">Player Name: </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Sideber;
